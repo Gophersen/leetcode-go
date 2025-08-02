fd1061326b3db6dc62009b22dce85f3d0d9813bc
Mohsen Khodabakhshi <gophersen@proton.me>
2025-08-02 15:58:04 +0330

    valid parentheses solved and added

---
+++ easy/020-valid-parentheses.go
@@ -0,0 +1,23 @@
+package main
+
+func isValid(s string) bool {
+	stack := []rune{}
+	hash := map[rune]rune{')': '(', ']': '[', '}': '{'}
+
+	for _, char := range s {
+		if match, found := hash[char]; found {
+			if len(stack) > 0 && stack[len(stack)-1] == match {
+				stack = stack[:len(stack)-1]
+			} else {
+				return false
+			}
+		} else {
+			stack = append(stack, char)
+		}
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	println(isValid("[()]"))
+}
